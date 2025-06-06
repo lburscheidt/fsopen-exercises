62c928bace332d66a4058151a7b47c454689f5a8
lburscheidt <burscheidt@zohomail.eu>
2025-06-06 19:37:37 +0200

    Complete exercise 3.14

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,30 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
-const mongoose = require("mongoose");
+const Entry = require("./models/entry");
 
 const app = express();
 const cors = require("cors");
-
-const password = process.argv[2];
-const url = `mongodb+srv://burscheidt:${password}@cluster0.pmxvhmu.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
-
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
-
-const entrySchema = new mongoose.Schema({
-	name: String,
-	number: String,
-});
-
-const Entry = mongoose.model("Entry", entrySchema);
-
-entrySchema.set("toJSON", {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString();
-		returnedObject._id = undefined;
-		returnedObject.__v = undefined;
-	},
-});
 
 morgan.token("id", function getId(req) {
 	return req.id;
@@ -39,29 +20,6 @@ app.use(
 	morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 
-/*let entries = [
-	{
-		id: "1",
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: "2",
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: "3",
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: "4",
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-];*/
-
 app.get("/", (request, response) => {
 	response.send("<h1>Hello World!</h1>");
 });
@@ -73,13 +31,9 @@ app.get("/api/entries", (request, response) => {
 });
 
 app.get("/api/entries/:id", (request, response) => {
-	const id = request.params.id;
-	const entry = entries.find((entry) => entry.id === id);
-	if (entry) {
+	Entry.findById(request.params.id).then((entry) => {
 		response.json(entry);
-	} else {
-		response.status(404).end();
-	}
+	});
 });
 
 app.delete("/api/entries/:id", (request, response) => {
@@ -88,10 +42,6 @@ app.delete("/api/entries/:id", (request, response) => {
 
 	response.status(204).end();
 });
-
-const generateId = () => {
-	return String(Math.floor(Math.random() * 100));
-};
 
 app.get("/info", (request, response) => {
 	const date = new Date();
@@ -115,24 +65,17 @@ app.post("/api/entries", (request, response) => {
 		});
 	}
 
-	if (entries.find((entry) => entry.name === body.name)) {
-		return response.status(400).json({
-			error: "name must be unique",
-		});
-	}
-
-	const entry = {
+	const entry = new Entry({
 		name: body.name,
 		number: body.number,
-		id: generateId(),
-	};
+	});
 
-	entries = entries.concat(entry);
-
-	response.json(entry);
+	entry.save().then((savedEntry) => {
+		response.json(savedEntry);
+	});
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
