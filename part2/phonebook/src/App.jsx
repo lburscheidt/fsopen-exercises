5b4cc8bbbb59629a5a5947b4e34c466f1a998379
lburscheidt <burscheidt@zohomail.eu>
2025-06-06 12:03:47 +0200

    Complete exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
 
 const App = () => {
-	const [entries, setEntries] = useState([{ name: "Arto Hellas" }]);
+	const [entries, setEntries] = useState([
+		{ name: "Arto Hellas", number: "040-123456", id: 1 },
+		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
+		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+	]);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
 
 	const handleNameChange = (e) => {
 		setNewName(e.target.value);
 	};
 
+	const handleNumberChange = (e) => {
+		setNewNumber(e.target.value);
+	};
+
 	const addEntry = (e) => {
 		e.preventDefault();
-		const newEntry = { name: newName };
+		const newEntry = { name: newName, number: newNumber };
 		const existingEntry = entries.find((entry) => entry.name === newEntry.name);
 		if (existingEntry) {
 			alert(`${newName} is already in the phonebook`);
@@ -18,6 +28,7 @@ const App = () => {
 		} else {
 			setEntries(entries.concat(newEntry));
 			setNewName("");
+			setNewNumber("");
 			console.log(entries);
 		}
 	};
@@ -26,18 +37,38 @@ const App = () => {
 		<div>
 			<h2>Phone book</h2>
 			<form onSubmit={addEntry}>
-				<div>
-					name: <input value={newName} onChange={handleNameChange} />
-				</div>
+				<table>
+					<tbody>
+						<tr>
+							<td>name:</td>
+							<td>
+								<input value={newName} onChange={handleNameChange} />
+							</td>
+						</tr>
+						<tr>
+							<td>number:</td>
+							<td>
+								<input value={newNumber} onChange={handleNumberChange} />
+							</td>
+						</tr>
+					</tbody>
+				</table>
+
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-
-			{entries.map((entry) => (
-				<p key={entry.name}>{entry.name}</p>
-			))}
+			<table>
+				<tbody>
+					{entries.map((entry) => (
+						<tr key={entry.name}>
+							<td> {entry.name}</td>
+							<td>{entry.number}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</div>
 	);
 };
