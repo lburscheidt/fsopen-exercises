69c31bfa4871cd1c693f5d9513d4225e8bae9dbf
lburscheidt <burscheidt@zohomail.eu>
2025-06-06 10:19:34 +0200

    Complete exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -31,16 +31,32 @@ const App = () => {
 		setVotes(votesCopy);
 	};
 
+	const indexOfMax = (arr) => {
+		return arr.reduce(
+			(maxIndex, elem, i, arr) => (elem > arr[maxIndex] ? i : maxIndex),
+			0,
+		);
+	};
+
 	return (
 		<div>
+			<h1>Anecdote of the day</h1>
 			<p>{anecdotes[selected]}</p>
-			<p>has {votes[selected]} {votes[selected] === 1 ? "vote" : "votes"}</p>
+			<p>
+				has {votes[selected]} {votes[selected] === 1 ? "vote" : "votes"}
+			</p>
 			<button type="button" onClick={voteForAnecdote}>
 				vote
 			</button>
 			<button type="button" onClick={selectAnecdote}>
 				next anecdote
 			</button>
+			<h1>Anecdote with most votes</h1>
+			<p>{anecdotes[indexOfMax(votes)]}</p>
+			<p>
+				has {votes[indexOfMax(votes)]}{" "}
+				{votes[indexOfMax(votes)] === 1 ? "vote" : "votes"}
+			</p>
 		</div>
 	);
 };
