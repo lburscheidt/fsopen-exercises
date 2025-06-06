8f09b0be5423f31033cddb1ad4b04f87a66c487c
lburscheidt <burscheidt@zohomail.eu>
2025-06-06 11:06:22 +0200

    Complete exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,45 +1,4 @@
-const Course = ({ course }) => {
-	return (
-		<>
-			<Header course={course.name} />
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
-		</>
-	);
-};
-
-const Header = ({ course }) => {
-	return <h1>{course}</h1>;
-};
-
-const Content = ({ parts }) => {
-	return (
-		<>
-			{parts.map((part) => (
-				<Part key={part.id} name={part.name} exercises={part.exercises} />
-			))}
-		</>
-	);
-};
-const Part = ({ name, exercises }) => {
-	return (
-		<p>
-			{name} {exercises}
-		</p>
-	);
-};
-
-const Total = ({ parts }) => {
-	const total = parts.reduce((accumulator, part) => {
-		return accumulator + part.exercises;
-	}, 0);
-	return (
-		<p>
-			<strong>Total of {total} exercises</strong>
-		</p>
-	);
-};
-
+import Course from "./Course";
 const App = () => {
 	const courses = [
 		{
