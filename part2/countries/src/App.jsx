46508dcfac79f1808d1f9999cc223c85bd214f27
lburscheidt <burscheidt@zohomail.eu>
2025-06-06 15:15:06 +0200

    Complete exercise 2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import countryService from "./services/countries";
+import CountriesList from "./components/CountriesList";
+import Filter from "./components/Filter";
+
+const App = () => {
+	const [searchInput, setSearchInput] = useState("");
+	const [countries, setCountries] = useState([]);
+
+	useEffect(() => {
+		countryService.getAll().then((countries) => {
+			setCountries(countries);
+		});
+	}, []);
+
+	const handleSearchChange = (event) => {
+		console.log(event.target.value);
+		setSearchInput(event.target.value);
+	};
+
+	const filteredCountries =
+		searchInput !== ""
+			? countries.filter((c) =>
+					c.name.common.toLowerCase().includes(searchInput.toLowerCase()),
+				)
+			: countries;
+
+	return (
+		<>
+			<Filter
+				handleSearchChange={handleSearchChange}
+				searchInput={searchInput}
+			/>
+			<CountriesList
+				searchInput={searchInput}
+				filteredCountries={filteredCountries}
+			/>
+		</>
+	);
+};
+
+export default App;
