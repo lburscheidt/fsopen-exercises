afdf395b7995874a76330c1297c09d5de8078d62
lburscheidt <burscheidt@zohomail.eu>
2025-06-06 12:55:45 +0200

    Complete exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = (props) => {
 	return (
@@ -60,12 +61,7 @@ const Entry = ({ entry }) => {
 	);
 };
 const App = () => {
-	const [entries, setEntries] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+	const [entries, setEntries] = useState([]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [filter, setFilter] = useState("");
@@ -100,6 +96,13 @@ const App = () => {
 			console.log(entries);
 		}
 	};
+
+	useEffect(() => {
+		console.log("effect");
+		axios.get("http://localhost:3001/entries").then((entriesFromServer) => {
+			setEntries(entriesFromServer.data);
+		});
+	}, []);
 
 	return (
 		<div>
